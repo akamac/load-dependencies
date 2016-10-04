3c45cf29d8d7f42856d9b0c9952237531c403ba4
Alexey Miasoedov <amiasoedov@intermedia.net>
2016-10-04 21:25:10 +0300

    add chocolatey support

--- load-dependencies.ps1
+++ load-dependencies.ps1
@@ -1,11 +1,10 @@
-[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='High')]
-param()
-
-$PSDefaultParameterValues = $Global:PSDefaultParameterValues
-
 try {
 	$ModuleManifest = Test-ModuleManifest $PSScriptRoot\*.psd1
-	Get-Module $ModuleManifest.Name | Remove-TypeData
+	Get-Module $ModuleManifest.Name | % {
+		$_.ExportedTypeFiles | % {
+			Remove-TypeData -Path $_
+		}
+	}
 	# for msi modules like VMware
 	$ModuleManifest.RequiredModules | % {
 		Import-Module -Name $_.Name -RequiredVersion $_.Version -ea Stop
@@ -18,47 +17,54 @@ try {
 				#nuget:Microsoft.Exchange.WebServices/2.2#nuget.org
 				$Destination = Join-Path $RequiredPackage.Destination "$PackageName.$Version"
 				if (-not (Test-Path $Destination)) {
-					Write-Verbose "Package $PackageName version $Version cannot be found"
-					if ($PSCmdlet.ShouldProcess("$PackageName $Version","Install from $Source")) {
-						Find-Package -ProviderName $ProviderName -Name $PackageName -RequiredVersion $Version -Source $Source |
-						Install-Package -Destination $_.Destination
-					} else {
-						throw "Package $PackageName version $Version is not installed"
-					}
+					Write-Verbose "Installing package $PackageName version $Version from source $Source"
+					Install-Package -ProviderName $ProviderName -Name $PackageName -RequiredVersion $Version -Source $Source -Destination $_.Destination
 				}
 				Write-Verbose 'Loading assemblies'
 				$RequiredPackage.RequiredAssemblies | % {
 					Add-Type -Path (Join-Path $Destination $_)
 				}
 				#$Package.PackageFilename -replace '\.nupkg$'
+			}
+			'powershellget|gitlab' {
+				#powershellget:PSScriptAnalyzer/1.5.0#PSGallery
+				#gitlab:Networking/1.2.0#Intermedia
+				$Module = Get-Module -Name $PackageName -ListAvailable | ? Version -eq $Version
+				if (-not $Module) {
+					Write-Verbose "Installing module $PackageName version $Version from source $Source"
+					Import-PackageProvider $ProviderName
+					switch ($ProviderName) {
+						powershellget {
+							Install-Module -Name $PackageName -RequiredVersion $Version -Repository $Source
+						}
+						gitlab {
+							$Credential = Get-Credential -Message "Provider $ProviderName source $Source"
+							Install-Package -ProviderName $ProviderName -Credential $Credential -Name $PackageName -RequiredVersion $Version -Source $Source
+						}
+					}
+					Import-Module -Name $PackageName -RequiredVersion $Version -WarningAction SilentlyContinue -ea Stop
+				}
+			}
+			'chocolatey' {
+				#chocolatey:OpenSSL.Light/1.1.0.20160926#
+				# until Chocolatey provider did not go GA
+				$Package = choco list $PackageName --local-only
+				if (-not $Package -match [regex]::Escape($PackageName)) {
+					Write-Verbose "Installing package $PackageName version $Version from source $Source"
+					choco install -y $PackageName
+				}
+				$Destination = $RequiredPackage.Destination
+				#$Package = Get-Package -Name $PackageName -RequiredVersion $Version -ProviderName $ProviderName
+				#if (-not $Package) {
+				#	Install-Package -Name $PackageName -RequiredVersion $Version -ProviderName $ProviderName
+				#}
+			}
+			'.*' {
 				if ($env:Path -notlike "*$Destination*" -and $RequiredPackage.EnvPath) {
 					Write-Verbose 'Updating $Path'
 					$MachinePath = [System.Environment]::GetEnvironmentVariable('Path','Machine')
 					[System.Environment]::SetEnvironmentVariable('Path',$MachinePath + ";$Destination",'Machine')
 					$env:Path = [System.Environment]::GetEnvironmentVariable('Path','Machine')
-				}
-			}
-			'powershellget|gitlab' {
-				#powershellget:PSScriptAnalyzer/1.5.0#PSGallery
-				#gitlab:Networking/1.2.0#Intermedia
-				$Module = Get-Module -FullyQualifiedName @{ModuleName = $PackageName; ModuleVersion = $Version} -ListAvailable
-				if (-not $Module) {
-					Write-Verbose "Module $PackageName version $Version cannot be found"
-					if ($PSCmdlet.ShouldProcess("$PackageName $Version","Install from $Source")) {
-						Import-PackageProvider $ProviderName
-						switch ($ProviderName) {
-							powershellget {
-								Install-Module -Name $PackageName -RequiredVersion $Version -Repository $Source
-							}
-							gitlab {
-								$Credential = Get-Credential -Message "Provider $ProviderName Source $Source"
-								Find-Package -ProviderName $ProviderName -Credential $Credential -Name $PackageName -RequiredVersion $Version | Install-Package
-							}
-						}
-					} else {
-						throw "Package $PackageName version $Version is not installed"
-					}
-					Import-Module -Name $PackageName -RequiredVersion $Version -ea Stop
 				}
 			}
 		}
